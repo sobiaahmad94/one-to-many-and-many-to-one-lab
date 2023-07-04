708e6901255cc7085643092e8bb66fb742321eff
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-07-04 16:47:34 +0100

    first commit

---
+++ src/main/java/com/codeclan/filepersonfolderlab/FilePersonFolderLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.filepersonfolderlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilePersonFolderLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilePersonFolderLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/filepersonfolderlab/FilePersonFolderLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.filepersonfolderlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilePersonFolderLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
